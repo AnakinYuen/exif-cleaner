a0f96280d75d376b4c2574341faf7a3cc77bdc94
Anakin Yuen <anakinyuen1024@gmail.com>
2020-02-18 11:01:28 +0800

    :star: feature: add logic for stripping exif data

---
+++ src/exifCleaner.ts
@@ -0,0 +1,37 @@
+import { isIOS } from './utils';
+
+const exifCleaner = async (files: File[]): Promise<Map<File, Blob>> => {
+  const map = new Map<File, Blob>();
+  if (isIOS) {
+    // Since iOS will always strip EXIF from uploaded photos, just return image url directly
+    files.forEach(file => {
+      map.set(file, file);
+    });
+  } else {
+    // drawImage to canvas to strip EXIF
+    const blobs = await Promise.all(
+      files.map(async file => {
+        const imageBitmap = await createImageBitmap(file);
+        const canvas = document.createElement('canvas');
+        canvas.width = imageBitmap.width;
+        canvas.height = imageBitmap.height;
+        const ctx = canvas.getContext('bitmaprenderer');
+        if (ctx) {
+          // transfer the ImageBitmap to it
+          ctx.transferFromImageBitmap(imageBitmap);
+        } else {
+          canvas.getContext('2d').drawImage(imageBitmap, 0, 0);
+        }
+        return await new Promise<Blob>(resolve => {
+          canvas.toBlob(resolve);
+        });
+      }),
+    );
+    files.forEach((file, index) => {
+      map.set(file, blobs[index]);
+    });
+  }
+  return map;
+};
+
+export default exifCleaner;
---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Use `navigator.platform` and `navigator.maxTouchPoints` to detect device type
+ *
+ * - [Detect if device is iOS](https://stackoverflow.com/questions/9038625/detect-if-device-is-ios)
+ * - [How to detect iPad and iPad OS version in iOS 13 and Up?](https://stackoverflow.com/questions/57765958/how-to-detect-ipad-and-ipad-os-version-in-ios-13-and-up)
+ */
+export const isIOS =
+  !!navigator.platform &&
+  /iPad|iPhone|iPod|MacIntel/.test(navigator.platform) &&
+  navigator.maxTouchPoints > 1;
