003cdd676e04927f7d9ba65638347b444607e96c
Anakin Yuen <anakinyuen1024@gmail.com>
2020-02-20 09:52:38 +0800

    :star: feature: add drag & drop feature

--- src/components/UploadBox/index.tsx
+++ src/components/UploadBox/index.tsx
@@ -1,12 +1,78 @@
 import { h, JSX } from 'preact';
-import { memo } from 'preact/compat';
+import { useState, useMemo } from 'preact/hooks';
+import style from './style.module.scss';
+import ImageIcon from '../ImageIcon';
+import { classNames } from '../../utils';
 
 const onChange = (e: Event): void => {
   console.log((e.target as HTMLInputElement).files);
 };
 
-const UploadBox = (): JSX.Element => {
-  return <input type="file" accept="image/*" multiple onChange={onChange} />;
+const preventDefaultAndStopPropagation = (e: Event) => {
+  e.preventDefault();
+  e.stopPropagation();
 };
 
-export default memo(UploadBox);
+interface Props {
+  close?: boolean;
+  className?: string;
+}
+
+const UploadBox = (props: Props): JSX.Element => {
+  const [isDragOver, setIsDragOver] = useState(false);
+
+  return useMemo(() => {
+    const dragOverHandler = (e: DragEvent) => {
+      setIsDragOver(true);
+      preventDefaultAndStopPropagation(e);
+    };
+
+    const dragLeaveHandler = (e: DragEvent) => {
+      setIsDragOver(false);
+      preventDefaultAndStopPropagation(e);
+    };
+
+    const dropHandler = (e: DragEvent) => {
+      console.log(e.dataTransfer.files);
+      dragLeaveHandler(e);
+    };
+
+    return (
+      <div
+        onDrag={preventDefaultAndStopPropagation}
+        onDragStart={preventDefaultAndStopPropagation}
+        onDragEnd={dragLeaveHandler}
+        onDragOver={dragOverHandler}
+        onDragEnter={dragOverHandler}
+        onDragLeave={dragLeaveHandler}
+        onDrop={dropHandler}
+        className={classNames(style.area, props.className, isDragOver && style['area--drag'])}
+      >
+        <input
+          className={style.input}
+          id="file"
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={onChange}
+        />
+        <div className={classNames(style.content, props.close && style['content--close'])}>
+          <ImageIcon
+            className={classNames(style.icon, isDragOver && style['icon--close'])}
+            close={isDragOver}
+          />
+          <p className={style.header}>Upload images</p>
+          <span className={style.content}>Drag and drop your images here to start the upload</span>
+          <label
+            className={classNames(style.button, isDragOver && style['button--hidden'])}
+            for="file"
+          >
+            Choose Images
+          </label>
+        </div>
+      </div>
+    );
+  }, [props.className, props.close, isDragOver, setIsDragOver]);
+};
+
+export default UploadBox;
