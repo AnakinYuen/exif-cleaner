a090d0cca5288b0b8ead02df114a3859cfd989ba
Anakin Yuen <anakinyuen1024@gmail.com>
2020-02-22 00:55:32 +0800

    :heavy_plus_sign: add: add service worker for Workbox to build PWA

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import { h, render, JSX } from 'preact';
 import App from './components/App';
 import { ContextStore, initState, reducer } from './globalState';
 import useThunkReducer from './utils/useThunkReducer';
+const SW_PATH = '/service-worker.js';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(SW_PATH);
+  });
+}
 
 const Provider = (): JSX.Element => {
   const [state, dispatch] = useThunkReducer(reducer, initState);
